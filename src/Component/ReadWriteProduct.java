f9204a09d5759c838680b7a53e0e2058a340bf76
phamtung86 <139665713+phamtung86@users.noreply.github.com>
2024-05-16 17:26:33 +0700

    [add] them chuc nang them san pham

--- src/Component/ReadWriteProduct.java
+++ src/Component/ReadWriteProduct.java
@@ -1,40 +1,27 @@
-
 package Component;
 
 import Models.Product;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class ReadWriteProduct {
-
+    ListProduct listProduct = new ListProduct();
     public ReadWriteProduct() {
     }
     Product pd;
 
-
     public void writeFile(Product product, String fileName) {
         try (FileWriter fw = new FileWriter(fileName, true); BufferedWriter bw = new BufferedWriter(fw); PrintWriter pw = new PrintWriter(bw)) {
-
-//            for (Product product : danhsachsanpham) {
             pw.println(product.getProductID());
             pw.println(product.getProductName());
             pw.println(product.getProductCategory());
             pw.println(product.getProductStock());
             pw.println(product.getProductPrice());
-
-//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,7 +37,9 @@ public class ReadWriteProduct {
                 p.setProductCategory(br.readLine());
                 p.setProductStock(br.readLine());
                 p.setProductPrice(Float.parseFloat(br.readLine()));
-                danhsachsanpham.add(p);
+                if(!listProduct.correctProduct(line, danhsachsanpham)){
+                    danhsachsanpham.add(p);
+                } 
 
             }
         } catch (IOException | NumberFormatException e) {
